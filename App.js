f18e907e64085cbff834faf497623250d938d2c1
delta <h0568@naver.com>
2021-05-30 22:00:19 +0900

    safeAreaView

--- App.js
+++ App.js
@@ -1,60 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text,View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
-class CounterButton extends React.Component {
-  static defaultProps = {
-    counter: -1,
-  };
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      counter: props.counter,
-    };
-  }
-
-  render() {
-    const clickHandler = () => {
-      this.setState({
-        counter: this.state.counter + 1,
-      });
-    };
-
-    return (
-      <View style={styles.buttonContainer}>
-          <Text>{this.state.counter}</Text>
-          <Button title={'click me!'} onPress={clickHandler}/>
-      </View>
-    );
-  }
-}
+// after `expo install react-native-safe-area-context`.
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <CounterButton counter={0} />
-        <CounterButton />
-        <CounterButton counter={2} />
-        <CounterButton counter={3} />
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaView style={styles.fullscreen}>
+        <View style={styles.container}>
+          <Image source={{
+              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+              width: 128,
+              height: 128,
+            }} />
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaView>
     );
   }
  }
 
 const styles = StyleSheet.create({
+  fullscreen: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    borderColor: 'red',
+    borderWidth: 2,
     justifyContent: 'center',
-  },
-  buttonContainer: {
     alignItems: 'center',
-    width: '100%',
-    borderWidth: 1,
-    borderColor: 'blue',
   },
 });
