71997a60ed06c651083621f733768962e85089a4
delta <h0568@naver.com>
2021-05-31 08:12:46 +0900

    citylist추가

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, ScrollView, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+import CityList from './CityList';
 
 // after `expo install react-native-safe-area-context`.
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -9,23 +10,7 @@ export default class App extends React.Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.container}>
-          <View style={styles.item}>
-            <Text style={styles.text}>Apple</Text>
-          </View>
-          <View style={styles.item}>
-            <Text style={styles.text}>Banana</Text>
-          </View>
-          <View style={styles.item}>
-            <Text style={styles.text}>Cherries</Text>
-          </View>
-          <View style={styles.item}>
-            <Text style={styles.text}>Damson plum</Text>
-          </View>
-          <View style={styles.item}>
-            <Text style={styles.text}>...</Text>
-          </View>
-        </ScrollView>
+        <CityList/>
         <StatusBar style="auto" />
       </SafeAreaView>
     );
@@ -36,15 +21,4 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-    item: {
-      flex: 1,
-      height: 50,
-      justifyContent: 'center',
-      borderWidth: 1,
-      borderColor: 'orange',
-    },
-    text: {
-      fontSize: 20,
-      textAlign: 'center',
-    },
 });
