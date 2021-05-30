f2f2df4067375b5b658f46f7c5f2de4878efc2a4
delta <h0568@naver.com>
2021-05-30 19:31:18 +0900

    count 추가

---
+++ App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, Text,View } from 'react-native';
+
+export default class App extends React.Component {
+  state = {
+    counter: 0,
+  };
+
+  render() {
+    const clickHandler = () => {
+      this.setState({
+        counter: this.state.counter + 1,
+      });
+    };
+
+    return (
+      <View style={styles.container}>
+          <Text>{this.state.counter}</Text>
+          <Button title={'click me!'} onPress={clickHandler}/>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+    textBig: {
+      fontSize: 32,
+    },
+    textRed: {
+      color: 'red'
+    },
+});
