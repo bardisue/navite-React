eb672f4055918350d9e1d4f0915555060b1e51f4
delta <h0568@naver.com>
2021-05-31 07:49:40 +0900

    render-list

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, Text, ScrollView, View } from 'react-native';
 
 // after `expo install react-native-safe-area-context`.
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -8,30 +8,43 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default class App extends React.Component {
   render() {
     return (
-      <SafeAreaView style={styles.fullscreen}>
-        <View style={styles.container}>
-          <Image source={{
-              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-              width: 128,
-              height: 128,
-            }} />
-          <StatusBar style="auto" />
-        </View>
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
+          <View style={styles.item}>
+            <Text style={styles.text}>Apple</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.text}>Banana</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.text}>Cherries</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.text}>Damson plum</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.text}>...</Text>
+          </View>
+        </ScrollView>
+        <StatusBar style="auto" />
       </SafeAreaView>
     );
   }
  }
 
 const styles = StyleSheet.create({
-  fullscreen: {
-    flex: 1,
-  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    borderColor: 'red',
-    borderWidth: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
+    item: {
+      flex: 1,
+      height: 50,
+      justifyContent: 'center',
+      borderWidth: 1,
+      borderColor: 'orange',
+    },
+    text: {
+      fontSize: 20,
+      textAlign: 'center',
+    },
 });
